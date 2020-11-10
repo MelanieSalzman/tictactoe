3c7d5920fd2d8b49714b2f24578c454394fb2864
Melanie Salzman <salzmanmelanie@gmail.com>
2020-11-10 16:36:37 -0300

    FIX: simplify function
    Took out Square render from a function.

--- src/components/game.js
+++ src/components/game.js
@@ -6,69 +6,71 @@ export const Game = () => {
 
     const [stepNumber, setStepNumber] = useState(0);
     const [xIsNext, setXIsNext] = useState(true);
-    const [history, setHistory] = useState([{squares: Array(9).fill(null)}])
+    const [history, setHistory] = useState([{ squares: Array(9).fill(null) }])
 
-const handleClick = (i) => {
-    const historyCopy = history.slice(0, stepNumber + 1);
-    const current = historyCopy[historyCopy.length - 1];
-    const squaresCopy = current.squares.slice();
-    if (calculateWinner(squaresCopy) || squaresCopy[i]) {
-        return;
+    const handleClick = (i) => {
+
+        const historyCopy = history.slice(0, stepNumber + 1);
+        const current = historyCopy[historyCopy.length - 1];
+        const squaresCopy = current.squares.slice();
+
+        if (calculateWinner(squaresCopy) || squaresCopy[i]) {
+            return;
+        }
+
+        squaresCopy[i] = xIsNext ? 'X' : 'O';
+
+        setHistory(historyCopy.concat([{ squares: squaresCopy }]))
+        setStepNumber(historyCopy.length)
+        setXIsNext(!xIsNext)
     }
-    squaresCopy[i] = xIsNext ? 'X' : 'O';
-    setHistory(historyCopy.concat([{
-        squares: squaresCopy
-    }]))
-    setStepNumber(historyCopy.length)
-    setXIsNext(!xIsNext)
-}
 
-const jumpTo = (step) => {
+    const jumpTo = (step) => {
+        setStepNumber(step)
+        setXIsNext((step % 2) === 0)
+    }
 
-    setStepNumber(step)
-    setXIsNext((step % 2) === 0)
-}
+    const historyCopy = history;
+    const current = historyCopy[stepNumber];
+    const winner = calculateWinner(current.squares);
 
-const historyCopy = history;
-const current = historyCopy[stepNumber];
-const winner = calculateWinner(current.squares);
+    const moves = historyCopy.map((step, move) => {
+        const desc = move ?
+            'Go to move #' + move :
+            'Go to game start';
+        return (
+            <li key={move}>
+                <button onClick={() => jumpTo(move)}>{desc}</button>
+            </li>
+        );
+    });
 
-const moves = historyCopy.map((step, move) => {
-    const desc = move ?
-        'Go to move #' + move :
-        'Go to game start';
+    let status;
+    if (winner) {
+        status = 'Winner: ' + winner;
+    } else {
+        status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+    }
+
     return (
-        <li key={move}>
-            <button onClick={() => jumpTo(move)}>{desc}</button>
-        </li>
+        <div className="game">
+            <div className="game-board">
+                <Board
+                    squares={current.squares}
+                    onClick={(i) => handleClick(i)}
+                />
+            </div>
+            <div className="game-info">
+                <div>{status}</div>
+                <ol>{moves}</ol>
+            </div>
+        </div>
     );
-});
 
-let status;
-if (winner) {
-    status = 'Winner: ' + winner;
-} else {
-    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
-}
-
-return (
-    <div className="game">
-        <div className="game-board">
-            <Board
-                squares={current.squares}
-                onClick={(i) => handleClick(i)}
-            />
-        </div>
-        <div className="game-info">
-            <div>{status}</div>
-            <ol>{moves}</ol>
-        </div>
-    </div>
-);
-    
 }
 
 const calculateWinner = (squares) => {
+
     const lines = [
         [0, 1, 2],
         [3, 4, 5],
@@ -79,6 +81,7 @@ const calculateWinner = (squares) => {
         [0, 4, 8],
         [2, 4, 6],
     ];
+    
     for (let i = 0; i < lines.length; i++) {
         const [a, b, c] = lines[i];
         if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
