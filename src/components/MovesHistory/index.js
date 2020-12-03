e8eb8a3bcbb3be803b81f0d7300a11697bd209c9
Melanie Salzman <salzmanmelanie@gmail.com>
2020-12-03 20:22:37 -0300

    Improve loops of moves history

--- src/components/MovesHistory/index.js
+++ src/components/MovesHistory/index.js
@@ -2,38 +2,33 @@ import React from 'react'
 import './styles.scss'
 
 const MovesHistory = ({ historyCopy, jumpTo }) => {
-  const getCellsByRow = (init, qty) => {
-    const cells = []
-
-    for (let index = init; index < init + qty; index++) {
-      if (historyCopy[index]) {
-        cells.push(
-          <button key={index} onClick={() => jumpTo(index)} className={historyCopy.length - 1 === index ? 'moveSelected' : 'move'}>
-            {index.toString()}
-          </button>
-        )
-      }
-    }
-
-    return cells
+  const MovesRow = ({ rowIndex }) => {
+    return (
+      <div>
+        {[rowIndex, rowIndex + 1, rowIndex + 2].map(index => {
+          if (historyCopy[index]) {
+            return (
+              <button key={index} onClick={() => jumpTo(index)} className={historyCopy.length - 1 === index ? 'moveSelected' : 'move'}>
+                {index.toString()}
+              </button>
+            )
+          } else return null
+        })}
+      </div>
+    )
   }
 
   return (
-    <div className='table'>
+    <div className='history'>
       <h3>Moves history</h3>
       <div className='row'>
         {historyCopy[0] && <button key={0} onClick={() => jumpTo(0)} className={historyCopy.length - 1 === 0 ? 'moveSelected' : 'move'}>Go to game start</button>}
       </div>
-      <div className='row'>
-        {getCellsByRow(1, 3)}
-      </div>
-      <div className='row'>
-        {getCellsByRow(4, 3)}
-      </div>
-      <div className='row'>
-        {getCellsByRow(7, 3)}
-      </div>
+      {[1, 4, 7].map(index => (
+        <MovesRow key={index} rowIndex={index} className='movesRow' />
+      ))}
     </div>
   )
 }
+
 export default MovesHistory
