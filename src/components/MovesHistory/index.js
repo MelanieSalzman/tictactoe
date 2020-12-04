47c315cc503c86bf9eb3ec78440fc758331409e7
Melanie Salzman <salzmanmelanie@gmail.com>
2020-12-03 21:10:45 -0300

    Implement moves history sorting

--- src/components/MovesHistory/index.js
+++ src/components/MovesHistory/index.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './styles.scss'
+import ascSort from '../../assets/images/ascSort.png'
+import descSort from '../../assets/images/descSort.png'
+const descMoves = [9, 6, 3]
+const ascMoves = [1, 4, 7]
 
 const MovesHistory = ({ historyCopy, jumpTo }) => {
+  const [order, setOrder] = useState(true)
+
+  const squaresIndex = (index) => {
+    return order ? [index, index + 1, index + 2] : [index, index - 1, index - 2]
+  }
+
   const MovesRow = ({ rowIndex }) => {
     return (
       <div>
-        {[rowIndex, rowIndex + 1, rowIndex + 2].map(index => {
+        {squaresIndex(rowIndex).map(index => {
           if (historyCopy[index]) {
             return (
               <button key={index} onClick={() => jumpTo(index)} className={historyCopy.length - 1 === index ? 'moveSelected' : 'move'}>
@@ -22,9 +32,15 @@ const MovesHistory = ({ historyCopy, jumpTo }) => {
     <div className='history'>
       <h3>Moves history</h3>
       <div className='row'>
-        {historyCopy[0] && <button key={0} onClick={() => jumpTo(0)} className={historyCopy.length - 1 === 0 ? 'moveSelected' : 'move'}>Go to game start</button>}
+        <button onClick={() => setOrder(!order)} className='sortButton'>
+          <img src={order ? ascSort : descSort} alt='sort' className='sortImage' />
+        </button>
+        {historyCopy[0] &&
+          <button key={0} onClick={() => jumpTo(0)} className={historyCopy.length - 1 === 0 ? 'moveSelected' : 'move'}>
+            Go to game start
+          </button>}
       </div>
-      {[1, 4, 7].map(index => (
+      {(order ? ascMoves : descMoves).map(index => (
         <MovesRow key={index} rowIndex={index} className='movesRow' />
       ))}
     </div>
