0a4d1c3f0028aabcffb156e5e937553c3db39b59
Melanie Salzman <salzmanmelanie@gmail.com>
2020-12-09 20:56:36 -0300

    Implement files structure

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
+import Game from './pages/Game'
+import Login from './pages/Login'
+import Landing from './pages/Landing'
+import './styles.scss'
+
+function App () {
+  return (
+    <div>
+      <Switch>
+        <Route path='/game'>
+          <Game />
+        </Route>
+        <Route path='/login'>
+          <Login />
+        </Route>
+        <Route path='/'>
+          <Landing />
+        </Route>
+      </Switch>
+    </div>
+  )
+}
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>,
+  </Router>
+  ,
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
